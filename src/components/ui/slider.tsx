72381e063d1153d6ff6a184c944d17193056bce7
syedbarimanjan <syedbarimanjan@gmail.com>
2026-09-17 14:32:27 +0500

    add timer to index page

---
+++ src/components/ui/slider.tsx
@@ -0,0 +1,64 @@
+import { Slider as SliderPrimitive } from "@base-ui/react/slider"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Slider({
+  className,
+  defaultValue,
+  value,
+  min = 0,
+  max = 100,
+  ...props
+}: React.ComponentProps<typeof SliderPrimitive.Root>) {
+  const _values = React.useMemo(
+    () =>
+      Array.isArray(value)
+        ? value
+        : Array.isArray(defaultValue)
+          ? defaultValue
+          : [min, max],
+    [value, defaultValue, min, max],
+  )
+
+  return (
+    <SliderPrimitive.Root
+      data-slot="slider"
+      defaultValue={defaultValue}
+      value={value}
+      min={min}
+      max={max}
+      thumbAlignment="edge"
+      className={cn(
+        "data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full",
+        className,
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Control
+        data-slot="slider-control"
+        className="active:cursor-grabbing cursor-grab relative flex w-full touch-none select-none items-center data-disabled:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col"
+      >
+        <SliderPrimitive.Track
+          data-slot="slider-track"
+          className="relative grow overflow-hidden rounded-base bg-secondary-background border-2 border-border select-none data-[orientation=horizontal]:h-3 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-3"
+        >
+          <SliderPrimitive.Indicator
+            data-slot="slider-range"
+            className="active:cursor-grabbing cursor-grab bg-main select-none data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
+          />
+        </SliderPrimitive.Track>
+        {Array.from({ length: _values.length }, (_, index) => (
+          <SliderPrimitive.Thumb
+            data-slot="slider-thumb"
+            key={index}
+            className="active:cursor-grabbing cursor-grab block size-5 shrink-0 rounded-full border-2 border-border bg-white ring-offset-white transition-colors select-none has-focus-visible:outline-none has-focus-visible:ring-2 has-focus-visible:ring-ring has-focus-visible:ring-offset-2 data-disabled:pointer-events-none data-disabled:opacity-50"
+          />
+        ))}
+      </SliderPrimitive.Control>
+    </SliderPrimitive.Root>
+  )
+}
+
+export { Slider }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,65 @@
+import { Button } from '@/components/ui/button';
+import { Slider } from '@/components/ui/slider'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className="font-base h-full w-full bg-cover bg-center bg-no-repeat bg-[url('/public/b.jpg')]">
-    <h1 className='text-white text-7xl'>Blah</h1>
-  </div>
+  const [selectedMinutes,setSelectedMinutes] = useState<number[]>([30]);
+  const [secondsLeft,setSecondsLeft] = useState<number>(selectedMinutes[0]*60);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() =>{
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    if(isRunning && secondsLeft > 0){
+      timer = setTimeout(() => {
+        setSecondsLeft((prev) => prev -1);
+      }, 1000);
+    }
+    else if(secondsLeft === 0){
+      setIsRunning(false);
+    }
+    return () => {
+      if(timer) clearTimeout(timer);
+    }
+  }, [isRunning,secondsLeft])
+
+  const handleSliderChange = (newValues: readonly number[] | number) => {
+    if(!isRunning){
+      const value = Array.isArray(newValues) ? newValues[0] : (newValues as number);
+      setSelectedMinutes([value]);
+      setSecondsLeft(value*60);
+    }
+    // if(isRunning){
+      
+    // }
+  }
+
+  const formatTime = (totalSeconds: number) => {
+    const mins = Math.floor(totalSeconds / 60);
+    const secs = totalSeconds % 60;
+    return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+  }
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
+  }
+
+  return (
+    <div className="font-base h-full w-full bg-cover bg-center bg-no-repeat bg-[url('/b.jpg')] flex items-center p-96">
+      <div className="h-full w-full flex flex-col items-center justify-center">
+        <div>
+          <h2 className="text-white text-9xl">{formatTime(secondsLeft)}</h2>
+        </div>
+        <div className="w-full">
+          <Slider defaultValue={selectedMinutes} value={[Math.ceil(secondsLeft / 60)]} onValueChange={handleSliderChange} max={120} step={1}  />
+        </div>
+        <Button variant={'white'} onClick={toggleTimer}>{isRunning ? "Pause" : "Start"}</Button>
+      </div>
+    </div>
+  )
 }
